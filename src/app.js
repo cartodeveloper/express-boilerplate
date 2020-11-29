6301d1b7d65e6280d4facee121d6c1ba38f07d53
María Torrente <caritotorrente@gmail.com>
2020-11-29 18:40:21 -0500

    adding error handling middleware

--- src/app.js
+++ src/app.js
@@ -10,6 +10,21 @@ const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
 app.use(cors());
 
 module.exports = app;
